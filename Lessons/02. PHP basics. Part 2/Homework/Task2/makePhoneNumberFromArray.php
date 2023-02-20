5f7bc9095c47cf3730d1fe3597615888af48413e
Ilya Kosharskiy <elirector@gmail.com>
2023-02-20 10:36:12 +0300

    hook test

--- Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 //определи функцию и пропиши типы аргументов функции и возвращаемого значения
+function makePhonNumberFromArray(array $numbers): string {
+    return "+7 (917) 232-87-98";
+}
+
 
 // Раскомментируй для отладки
 //var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
