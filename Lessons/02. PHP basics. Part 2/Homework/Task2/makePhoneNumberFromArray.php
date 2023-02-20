e5876bddd112a9baf413f44fbd6e6579dae97551
DariaGazkaeva <d.gazkaeva@gmail.com>
2023-02-20 12:59:34 +0000

    lesson2.task2 done

--- Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
@@ -3,6 +3,14 @@
 declare(strict_types=1);
 
 //определи функцию и пропиши типы аргументов функции и возвращаемого значения
+function makePhoneNumberFromArray(array $numbers) : string {
+
+    $prefix = "+$numbers[0]";
+    $zoneCode = "($numbers[1]$numbers[2]$numbers[3])";
+    $clientNumber = "$numbers[4]$numbers[5]$numbers[6]-$numbers[7]$numbers[8]-$numbers[9]$numbers[10]";
+
+    return "$prefix $zoneCode $clientNumber";
+}
 
 // Раскомментируй для отладки
-//var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
+var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
