f93d59ca84e585442cfb5a322ccd209c3f92a632
Maria.Morozova <mmorozova31@mail.ru>
2023-02-20 17:15:08 +0300

    Task2 is done

--- Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 //определи функцию и пропиши типы аргументов функции и возвращаемого значения
+function makePhoneNumberFromArray(array $numbers): string {
+    $PhoneNumber = "+$numbers[0] ($numbers[1]$numbers[2]$numbers[3]) $numbers[4]$numbers[5]$numbers[6]-$numbers[7]$numbers[8]-$numbers[9]$numbers[10]";
+    return $PhoneNumber;
+}
 
 // Раскомментируй для отладки
-//var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
+var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
