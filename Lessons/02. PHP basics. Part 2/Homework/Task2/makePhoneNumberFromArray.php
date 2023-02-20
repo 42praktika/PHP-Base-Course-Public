064b7bdc0f1d8f8b51e82ccdec728e028ffb1cc3
Semen Savelev <ctvbxm@mail.ru>
2023-02-20 16:27:04 +0300

    [UPD] Type of function in Lesson 2 Task 2

--- Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-function makePhoneNumberFromArray($numbers)
+function makePhoneNumberFromArray($numbers): string
 {
     if (count($numbers) === 11) {
         return "+$numbers[0] ($numbers[1]$numbers[2]$numbers[3]) $numbers[4]$numbers[5]$numbers[6]-$numbers[7]$numbers[8]-$numbers[9]$numbers[10]";
