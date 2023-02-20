2d59303beb0ab1244d39bbf6b93df3397f8df7ce
KseniaKatargina <90481483+KseniaKatargina@users.noreply.github.com>
2023-02-20 16:09:41 +0300

    first task done

--- Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
@@ -3,6 +3,12 @@
 declare(strict_types=1);
 
 //определи функцию и пропиши типы аргументов функции и возвращаемого значения
-
+function makePhoneNumberFromArray($arrayOfNumbers = array()): string {
+    if(count($arrayOfNumbers) != 11){
+        return "Передано недостаточно цифр";
+    } else {
+        return "+"."$arrayOfNumbers[0] ($arrayOfNumbers[1]$arrayOfNumbers[2]$arrayOfNumbers[3]) $arrayOfNumbers[4]$arrayOfNumbers[5]$arrayOfNumbers[6]-$arrayOfNumbers[7]$arrayOfNumbers[8]-$arrayOfNumbers[9]$arrayOfNumbers[10]";
+    }
+}
 // Раскомментируй для отладки
-//var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
+var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
