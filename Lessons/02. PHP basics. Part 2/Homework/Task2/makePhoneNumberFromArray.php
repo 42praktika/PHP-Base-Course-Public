937a90c8542c2d86ca3e2bed0747318d9bd93093
KipeTBest <90268471+KipeTBest@users.noreply.github.com>
2023-02-20 16:02:28 +0300

    ClassWork
    Added 2nd task from classwork

--- Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
@@ -2,7 +2,40 @@
 
 declare(strict_types=1);
 
+function makePhoneNumberFromArray($array) {
+    $answer = "";
+    $flag = false;
+
+    for ($i = 0; $i <= 17; $i++) {
+        if ($i == 0) {
+            $answer .= "+";
+            $iter = 0;
+        } elseif ($i == 2 && !$flag) {
+            $answer .= " ";
+            $flag = true;
+        } elseif ($i == 8 && $flag) {
+            $answer .= " ";
+            $flag = true;
+        } elseif ($i == 3 && $flag) {
+            $answer .= "(";
+            $flag = true;
+        } elseif ($i == 7 && !$flag) {
+            $answer .= ")";
+            $flag = true;
+        } elseif (($i == 12 || $i == 15) && !$flag) {
+            $answer .= "-";
+            $flag = true;
+        } else {
+            $answer .= $array[$iter];
+            $flag = false;
+            if ($iter != 10) {
+                $iter++;
+            }
+        }
+    }
+    return $answer;
+}
 //определи функцию и пропиши типы аргументов функции и возвращаемого значения
 
 // Раскомментируй для отладки
-//var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8','7', '9', '8']));
+var_dump(makePhoneNumberFromArray(['7', '9', '1', '7', '2', '3', '2', '8', '7', '9', '8']));
