cccf457589f880907d03113502745c56c0095461
Safiullin.Ayaz <tony.holl2609@gmail.com>
2023-02-20 17:09:02 +0300

    [ADD] Task2

--- Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task2/makePhoneNumberFromArray.php
@@ -6,5 +6,3 @@ function makePhoneNumberFromArray($arguments): string
 {
     return "+$arguments[0] ($arguments[1]$arguments[2]$arguments[3]) $arguments[4]$arguments[5]$arguments[6]-$arguments[7]$arguments[8]-$arguments[9]$arguments[10]";
 }
-
-var_dump(makePhoneNumberFromArray(['7','7','7','7','7','7','7','7','7','7','7']));
