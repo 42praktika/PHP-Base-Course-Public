11d857c24ef7af83912591d2f5611f1d4d22470d
DariaGazkaeva <d.gazkaeva@gmail.com>
2023-02-24 17:27:38 +0000

    lesson2.task1 done

--- Lessons/02. PHP basics. Part 2/Homework/Task1/familyTreeNormalisation.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task1/familyTreeNormalisation.php
@@ -40,10 +40,42 @@ $familyTree = [
 
 function normalizeFamilyTree(array $familyTree): array
 {
-    //здесь должен быть твой код
-    return [];
+    $nameExists = array_key_exists('name', $familyTree);
+    $parentsExist = array_key_exists('parents', $familyTree) && count($familyTree['parents']) != 0;
+
+    if ($nameExists) {
+        $familyTree['name'] = normalizeName($familyTree['name']);
+    }
+
+    // если ключ parents существует и внутри что-то есть
+    if ($parentsExist) {
+        $motherExist = array_key_exists('mother', $familyTree['parents']);
+        $fatherExist = array_key_exists('father', $familyTree['parents']);
+
+        if (!$fatherExist && !$motherExist) {
+            // случай, когда ключи mother и father лежат не в parents, а в промежуточном массиве, который лежит в parents
+            $familyTree['parents'][0] = normalizeFamilyTree($familyTree['parents'][0]);
+        }
+
+        if ($motherExist) {
+            $familyTree['parents']['mother'] = normalizeFamilyTree($familyTree['parents']['mother']);
+        }
+        if ($fatherExist) {
+            $familyTree['parents']['father'] = normalizeFamilyTree($familyTree['parents']['father']);
+        }
+    // нормализуем данные в промежуточном массиве
+    } else if (array_key_exists('mother', $familyTree) || array_key_exists('father', $familyTree)) {
+        $familyTree['mother'] = normalizeFamilyTree($familyTree['mother']);
+        $familyTree['father'] = normalizeFamilyTree($familyTree['father']);
+    }
+    return $familyTree;
 }
 
+function normalizeName(string $name) : string
+{
+    $name = preg_replace('/\s+/', ' ', $name);
+    $name = mb_convert_case($name, MB_CASE_TITLE, "UTF-8");
+    return trim($name);
+}
 
-// Раскоммментируй для отладки
 //var_dump(normalizeFamilyTree($familyTree));
