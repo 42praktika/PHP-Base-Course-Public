cf4f1706f7bb076fc4bd106b20fd4b9e78e1bcb6
DeeUshakoff <dee.ushakoff@gmail.com>
2023-03-05 22:48:58 +0300

    02. PHP basics. Part 2, Task 1
    Added Homework

--- Lessons/02. PHP basics. Part 2/Homework/Task1/familyTreeNormalisation.php
+++ Lessons/02. PHP basics. Part 2/Homework/Task1/familyTreeNormalisation.php
@@ -38,12 +38,54 @@ $familyTree = [
 ];
 
 
-function normalizeFamilyTree(array $familyTree): array
+function normalizeFamilyTree(array &$familyTree): array
 {
-    //здесь должен быть твой код
-    return [];
+    foreach ($familyTree as &$item)
+    {
+        if(is_array($item))
+        {
+            normalizeFamilyTree($item);
+        }
+        else
+        {
+            $item = normalizeName($item);
+        }
+    }
+    return $familyTree;
 }
 
+function normalizeName(string $name)
+{
 
+    $filter = ['','    ',' ', null];
+    $name = preg_replace('/\d/','',$name);
+    $name = str_replace('	', ' ', $name);
+    $name = str_replace('   ', ' ', $name);
+
+
+    $splitted = explode(' ', $name);
+    $splitted = array_diff($splitted, $filter);
+    foreach ($splitted as &$item)
+    {
+
+        $item = mb_convert_case($item, MB_CASE_LOWER, 'UTF-8');
+        $item = mb_convert_case($item, MB_CASE_TITLE, 'UTF-8');
+
+        $subsplitted = explode('-', $item);
+        if(count($subsplitted) == 1)
+        {
+            continue;
+        }
+        foreach ($subsplitted as &$subitem)
+        {
+            $subitem = mb_convert_case($subitem, MB_CASE_LOWER, 'UTF-8');
+            $subitem = mb_convert_case($subitem, MB_CASE_TITLE, 'UTF-8');
+        }
+        $item = join('-', $subsplitted);
+    }
+
+    $name = join(" ", $splitted);
+    return $name;
+}
 // Раскоммментируй для отладки
 //var_dump(normalizeFamilyTree($familyTree));
