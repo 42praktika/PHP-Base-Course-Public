147ac756b3bfbdd3ab37e931e9c3476c4a593d40
AlsuKhairullina107 <94959980+AlsuKhairullina107@users.noreply.github.com>
2023-03-27 16:23:41 +0500

    homework

---
+++ Project/web/main.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grape</title>
+    <link rel = "stylesheet" href="assets/main.css">
+</head>
+<body>
+<div>виноград - это и поесть и попить</div>
+<p data-item='Grape'>Grape</p>
+
+<section>
+    <div>ресторан</div>
+    <nav>
+        <ul class="menuItems">
+            <li><a href='#' data-item='Menu'>Меню</a></li>
+            <li><a href='#' data-item='Booking'>Бронирование</a></li>
+            <li><a href='#' data-item='About'>О нас</a></li>
+            <li><a href='#' data-item='Contact'>Контакты</a></li>
+        </ul>
+    </nav>
+
+</section>
+
+<footer>
+
+    <div class="footer-copyright text-center"> г. Казань, ул. разведчика Сабитова телефон: +79179092384
+    </div>
+
+</footer>
+
+</body>
+</html>
