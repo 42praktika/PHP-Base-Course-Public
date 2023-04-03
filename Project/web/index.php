39f878b51c872c8d655ef6a7f64f1b263e7a0831
Elirector <elirector@gmail.com>
2023-04-03 15:17:04 +0300

    Project: Response status code

--- Project/web/index.php
+++ Project/web/index.php
@@ -3,8 +3,14 @@
 use app\controllers\PresentationController;
 use app\core\Application;
 
+//Возвращает файлы напрямую
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|html?|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;
+}
+
 const PROJECT_ROOT = __DIR__."/../";
 require "../vendor/autoload.php";
+require PROJECT_ROOT."/polyfill/http_send_status.php";
 spl_autoload_register(function ($className) {
    require str_replace("app\\",PROJECT_ROOT, $className).".php";
 
@@ -13,6 +19,7 @@ spl_autoload_register(function ($className) {
 $application = new Application();
 $router = $application->getRouter();
 
+$router->setGetRoute("/get500", "");
 $router->setGetRoute("/", [new PresentationController, "getView"]);
 $router->setPostRoute("/handle", [new PresentationController, "handleView"]);
 
